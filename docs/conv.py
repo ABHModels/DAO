92fbda9109f2191e3a2e835d530ee4725f9a2c7c
Astroyimin <1075444920@qq.com>
2025-12-10 15:29:54 +0800

    read doc

---
+++ docs/conv.py
@@ -0,0 +1,39 @@
+# docs/conf.py
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..')) 
+
+project = 'DAO'
+copyright = '2025, Yimin Huang'
+author = 'Yimin Huang'
+version = '0.1'
+
+
+extensions = [
+    'sphinx.ext.autodoc',      
+    'sphinx.ext.napoleon',      #
+    'sphinx.ext.mathjax',       #
+    'sphinx.ext.viewcode',      # 
+    'myst_parser',            
+]
+
+
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+
+    
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
---
+++ docs/index.rst
@@ -0,0 +1,26 @@
+Welcome to DAO's documentation!
+===============================
+
+**DAO** (Data Analysis for Objects?) is a new reflection model for black hole accretion disks, designed to be compatible with XSTAR.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: User Guide:
+
+   installation
+   quickstart
+   theory
+   parameters
+
+.. toctree::
+   :maxdepth: 2
+   :caption: API Reference:
+
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
